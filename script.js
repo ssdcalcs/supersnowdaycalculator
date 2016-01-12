d3eb6162391f0d7a86c768d14a819c4a1e261e6f
Jack <jap23@scasd.org>
2016-01-11 20:06:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function calcer() {
     return;
   } 
   else {
-  	prob = (6 * ((zipCode + (day * 12)) / 6)) % 100;
+  	prob = ((zipCode + (day * 12)) / 6) % 100;
   }
   
   alert("The snowday calculator predicts that there is a(n) " + prob +" percent chance.");
